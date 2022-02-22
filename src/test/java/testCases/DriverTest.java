86c416290f2154261e548c9e69ae8404f4b7df97
nikhiltejreddy <nikhilreddysurya387@gmail.com>
2022-02-22 16:14:26 +0530

    york.com test

--- src/test/java/testCases/DriverTest.java
+++ src/test/java/testCases/DriverTest.java
@@ -2,17 +2,19 @@ package testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class DriverTest {
 	
 	static WebDriver driver;
-		
+	@Test
 	public void fnBrowserStart() {
 			
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/src/main/resources/Drivers/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/src/main/resources/Drivers/chromedriver1.exe");
 			driver = new ChromeDriver();
 			driver.manage().window().maximize();
-			driver.get("https://www.york.com/");
+			driver.navigate().to("https://www.york.com/");
+			
 			}
 
 		
